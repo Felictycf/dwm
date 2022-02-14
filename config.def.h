64bfb62c717762c4efa0c8aad8b27d65fe242eac
aaron <wzj020109@163.com>
2022-02-14 13:25:35 +0800

    feat(config.def.h dwm.1 dwm.c): Add Auto-start

--- config.def.h
+++ config.def.h
@@ -57,7 +57,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", NULL };
+static const char *dmenucmd[] = { "/home/aaron/GitHub/suckless/dwm/scripts/menu.sh", NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
---
+++ scripts/menu.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+
+#################################################
+# File Name menu
+# Version 1.0
+# Author aaron
+# Email wzj020109@163.com
+# Created Time 2022-02-14
+# Desc 菜单脚本
+#################################################
+
+
+rofi -show drun
